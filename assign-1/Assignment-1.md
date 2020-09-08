91ba3de8cc870f6e24f75af1068445b01b21168c
madhat5 <davidmalary@gmail.com>
2020-09-08 14:13:15 -0400

    assign1 done

---
+++ assign-1/Assignment-1.md
@@ -0,0 +1,6 @@
+### Assignment 1
+
+- Install node, if needed
+- npm install request, if needed
+- node main.js to run
+
---
+++ assign-1/main.js
@@ -0,0 +1,38 @@
+// console.log('sim sim salabim')
+
+var request = require('request');
+var fs = require('fs');
+
+var n = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+
+var dataModel = {
+    url: '',
+    borough: '',
+    meetingZone: '',
+    meeting: {
+        location: '',
+        address: '',
+        hours: '',
+        notes: ''
+    }
+}
+
+var scrapePage = function(count) {
+    request('https://parsons.nyc/aa/m' + count + '.html', function(err, res, body){
+        if (!err && res.statusCode == 200) {
+            fs.writeFileSync('/home/ec2-user/environment/assign-1/data/aa_pg' + count + '.txt', body);
+            
+            console.log('site visited |', 'https://parsons.nyc/aa/m' + count + '.html')
+        }
+        else {console.log("Request failed!")}
+    });
+}
+
+for (var i = 0; i < n.length; i++) {
+    var count = n[i]
+    // console.log(count, +count)
+    // console.log('https://parsons.nyc/aa/m' + count + '.html')
+    
+    scrapePage(count)
+}
+
